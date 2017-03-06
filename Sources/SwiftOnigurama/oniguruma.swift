7089ef75c6001093435e34d0abf950a704ed81d0
Damian Malarczyk <damian.malarczyk@gmail.com>
2017-03-06 12:35:35 +0100

    C replace

--- Sources/SwiftOnigurama/oniguruma.swift
+++ Sources/SwiftOnigurama/oniguruma.swift
@@ -22,16 +22,31 @@ extension String {
     }
 }
 
+
+
 public class Regex {
+    static let ONIG_OPTION_EXTEND = ONIG_OPTION_IGNORECASE << 1
+    static let  ONIG_OPTION_MULTILINE    =         (ONIG_OPTION_EXTEND             << 1)
+    static let  ONIG_OPTION_SINGLELINE       =    (ONIG_OPTION_MULTILINE          << 1)
+    static let  ONIG_OPTION_FIND_LONGEST    =     (ONIG_OPTION_SINGLELINE         << 1)
+    static let  ONIG_OPTION_FIND_NOT_EMPTY     = (ONIG_OPTION_FIND_LONGEST       << 1)
+    static let  ONIG_OPTION_NEGATE_SINGLELINE   = (ONIG_OPTION_FIND_NOT_EMPTY     << 1)
+    static let  ONIG_OPTION_DONT_CAPTURE_GROUP =  (ONIG_OPTION_NEGATE_SINGLELINE  << 1)
+    static let  ONIG_OPTION_CAPTURE_GROUP     =   (ONIG_OPTION_DONT_CAPTURE_GROUP << 1)
+    static let  ONIG_OPTION_NOTBOL           =    (ONIG_OPTION_CAPTURE_GROUP << 1)
+    static let  ONIG_OPTION_NOTEOL          =     (ONIG_OPTION_NOTBOL << 1)
+    static let  ONIG_OPTION_POSIX_REGION   =      (ONIG_OPTION_NOTEOL << 1)
+    static var ONIG_ENCODING_ASCII = OnigEncodingASCII
+    static var ONIG_ENCODING_UTF8 = OnigEncodingUTF8
     public enum Encoding {
         case ascii, utf8
         
-        var onigEncoding: OnigEncodingType {
+        func onigEncoding() -> OnigEncoding {
             switch self {
             case .ascii:
-                return OnigEncodingASCII
+                return withUnsafeMutablePointer(to: &OnigEncodingASCII) { $0 }
             case .utf8:
-                return OnigEncodingUTF8
+                return withUnsafeMutablePointer(to: &OnigEncodingUTF8) { $0 }
             }
         }
     }
@@ -47,15 +62,16 @@ public class Regex {
     }
     
     var regex: OnigRegex? = OnigRegex.allocate(capacity: 1)
-
+    
+    var option: UInt32 = ONIG_OPTION_NONE
+    
     
     public init(pattern: String, encoding: Encoding = .ascii) throws {
         var r: Int32 = 0
         var info = OnigErrorInfo()
         
         pattern.withUCString { cStr in
-            var onigEncoding = encoding.onigEncoding
-            r = onig_new(&regex, cStr, cStr.advanced(by: pattern.characters.count), ONIG_OPTION_DEFAULT, &onigEncoding, OnigDefaultSyntax, &info)
+            r = onig_new(&regex, cStr, cStr.advanced(by: pattern.characters.count), option, encoding.onigEncoding(), &OnigSyntaxRuby, &info)
         }
         if r != ONIG_NORMAL {
             let message = UnsafeMutablePointer<UInt8>.allocate(capacity: Int(ONIG_MAX_ERROR_MESSAGE_LEN))
@@ -86,8 +102,8 @@ public class Regex {
             start = ptr
             end = ptr.advanced(by: str.characters.count)
             range = end
-            return onig_search(regex!, ptr, end, start, range, reg, ONIG_OPTION_NONE)
-
+            return onig_search(regex!, ptr, end, start, range, reg, option)
+            
         }
         
         onig_region_free(reg, 1)
@@ -115,7 +131,7 @@ public class Regex {
             start = ptr
             end = ptr.advanced(by: str.characters.count)
             range = end
-            return onig_search(regex!, ptr, end, start, range, reg, ONIG_OPTION_NONE)
+            return onig_search(regex!, ptr, end, start, range, reg, option)
             
         }
         defer {
@@ -139,21 +155,27 @@ public class Regex {
         guard let reg = onig_region_new() else {
             throw Error.region
         }
-        var start: UnsafePointer<UInt8>! = nil
-        var range: UnsafePointer<UInt8>! = nil
+        
         var end: UnsafePointer<UInt8>! = nil
         var res: Int32 = 0
         
         while res >= 0 {
+            
             res = str.withUCString { ptr in
-                start = ptr
-                end = ptr.advanced(by: str.characters.count)
-                range = end
-                return onig_search(regex!, ptr, end, start, range, reg, ONIG_OPTION_NONE)
+                str.withCString({ (cPtr) in
+                    end = ptr.advanced(by: Int(strlen(cPtr)))
+                })
+                
+                return onig_search(regex!, ptr, end, ptr, end, reg, option)
             }
             if res >= 0 {
                 
-                str.replaceSubrange(Range<String.Index>(uncheckedBounds:(str.index(str.startIndex, offsetBy: String.IndexDistance(reg.pointee.beg.pointee)), str.index(str.startIndex, offsetBy: String.IndexDistance(reg.pointee.end.pointee)))), with: with)
+                str.withCString { ptr in
+                    let end =  ptr.advanced(by: Int(reg.pointee.end.pointee))
+                    
+                    memmove(UnsafeMutableRawPointer(mutating: ptr.advanced(by: Int(reg.pointee.beg.pointee))), UnsafeMutableRawPointer(mutating: end), Int(strlen(end)) + 1)
+                    str = String(cString: ptr)
+                }
             }
         }
         onig_region_free(reg, 1)
